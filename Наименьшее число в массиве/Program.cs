98ff79c1eb81652013c44cde05222d52f68fd0f0
kunicind1996@gmail.com <kunicind1996@gmail.com>
2023-02-25 21:36:31 +0700

    Добавьте файлы проекта.

---
+++ Наименьшее число в массиве/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Сколько элементов в массиве ? ");
+int NewArray = int.Parse(Console.ReadLine());
+int[] Array = new int[NewArray];
+for (int i = 0;i < NewArray; i++)
+{
+    Console.WriteLine($"значение элемента массива: {i}");
+    Array[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Значения массива ");
+for (int i = 0; i < Array.Length; i++)
+{
+    
+    Console.WriteLine(Array[i]);
+}
+Console.Write("Значение минимального элемента массива : ");
+Console.WriteLine(Array.Min());
+Console.ReadLine();
